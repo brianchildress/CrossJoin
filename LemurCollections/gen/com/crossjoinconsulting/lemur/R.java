2b764e30a43effa8703e87104e0ed4f2c28bcc13
Brian Childress <brian.p.childress@crossjoinconsulting.com>
2011-08-14 16:03:31 -0400

    Google API

--- LemurCollections/gen/com/crossjoinconsulting/lemur/R.java
+++ LemurCollections/gen/com/crossjoinconsulting/lemur/R.java
@@ -10,13 +10,6 @@ package com.crossjoinconsulting.lemur;
 public final class R {
     public static final class attr {
     }
-    public static final class color {
-        public static final int status_text=0x7f040001;
-        public static final int status_view=0x7f040000;
-        public static final int transparent=0x7f040002;
-        public static final int viewfinder_frame=0x7f040003;
-        public static final int viewfinder_mask=0x7f040004;
-    }
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int sample_0=0x7f020001;
@@ -29,24 +22,18 @@ public final class R {
         public static final int sample_7=0x7f020008;
     }
     public static final class id {
-        public static final int auto_focus=0x7f050000;
-        public static final int benchmark_done=0x7f050001;
-        public static final int btnSubmit=0x7f05000a;
-        public static final int etEmail=0x7f050007;
-        public static final int etPassword=0x7f050009;
-        public static final int gvImages=0x7f05000d;
-        public static final int out_text=0x7f05000c;
-        public static final int quit=0x7f050002;
-        public static final int save=0x7f050003;
-        public static final int save_failed=0x7f050005;
-        public static final int save_succeeded=0x7f050004;
-        public static final int tvContents=0x7f050010;
-        public static final int tvEmail=0x7f050006;
-        public static final int tvError=0x7f05000b;
-        public static final int tvFormat=0x7f050011;
-        public static final int tvPwd=0x7f050008;
-        public static final int tvUserName=0x7f05000f;
-        public static final int tvWelcome=0x7f05000e;
+        public static final int btnSubmit=0x7f050004;
+        public static final int etEmail=0x7f050001;
+        public static final int etPassword=0x7f050003;
+        public static final int gvImages=0x7f050007;
+        public static final int out_text=0x7f050006;
+        public static final int tvContents=0x7f05000a;
+        public static final int tvEmail=0x7f050000;
+        public static final int tvError=0x7f050005;
+        public static final int tvFormat=0x7f05000b;
+        public static final int tvPwd=0x7f050002;
+        public static final int tvUserName=0x7f050009;
+        public static final int tvWelcome=0x7f050008;
     }
     public static final class layout {
         public static final int login=0x7f030000;
@@ -70,42 +57,36 @@ public final class R {
  limitations under the License.
  
          */
-        public static final int about_menu=0x7f060002;
-        public static final int about_message=0x7f060003;
-        public static final int app_name=0x7f060001;
-        public static final int benchmark_help=0x7f060005;
-        public static final int benchmark_name=0x7f060004;
-        public static final int benchmark_run=0x7f060006;
-        public static final int benchmark_running=0x7f060007;
-        public static final int encode_bad_data=0x7f060008;
-        public static final int encode_contact=0x7f060009;
-        public static final int encode_email=0x7f06000a;
-        public static final int encode_location=0x7f06000b;
-        public static final int encode_phone=0x7f06000c;
-        public static final int encode_sms=0x7f06000d;
-        public static final int encode_url=0x7f06000e;
-        public static final int get_camera_parameters=0x7f06000f;
-        public static final int hello=0x7f060000;
-        public static final int ok_button=0x7f060010;
-        public static final int parameters_message=0x7f060011;
-        public static final int result_failed=0x7f060012;
-        public static final int result_failed_why=0x7f060013;
-        public static final int result_succeeded=0x7f060014;
-        public static final int save_failed=0x7f060015;
-        public static final int save_succeeded=0x7f060016;
-        public static final int scan_anything=0x7f060017;
-        public static final int scan_product=0x7f060018;
-        public static final int scan_qr_code=0x7f060019;
-        public static final int search_book_contents=0x7f06001a;
-        public static final int share_via_barcode=0x7f06001b;
-        public static final int status_message=0x7f06001c;
-        public static final int take_test_photos=0x7f06001d;
+        public static final int about_menu=0x7f040003;
+        public static final int about_message=0x7f040004;
+        public static final int app_name=0x7f040001;
+        public static final int benchmark_help=0x7f040006;
+        public static final int benchmark_name=0x7f040005;
+        public static final int benchmark_run=0x7f040007;
+        public static final int benchmark_running=0x7f040008;
+        public static final int company=0x7f040002;
+        public static final int encode_bad_data=0x7f040009;
+        public static final int encode_contact=0x7f04000a;
+        public static final int encode_email=0x7f04000b;
+        public static final int encode_location=0x7f04000c;
+        public static final int encode_phone=0x7f04000d;
+        public static final int encode_sms=0x7f04000e;
+        public static final int encode_url=0x7f04000f;
+        public static final int get_camera_parameters=0x7f040010;
+        public static final int hello=0x7f040000;
+        public static final int ok_button=0x7f040011;
+        public static final int parameters_message=0x7f040012;
+        public static final int result_failed=0x7f040013;
+        public static final int result_failed_why=0x7f040014;
+        public static final int result_succeeded=0x7f040015;
+        public static final int save_failed=0x7f040016;
+        public static final int save_succeeded=0x7f040017;
+        public static final int scan_anything=0x7f040018;
+        public static final int scan_product=0x7f040019;
+        public static final int scan_qr_code=0x7f04001a;
+        public static final int search_book_contents=0x7f04001b;
+        public static final int share_via_barcode=0x7f04001c;
+        public static final int status_message=0x7f04001d;
+        public static final int take_test_photos=0x7f04001e;
     }
-    public static final class styleable {
-        /** Attributes that can be used with a ViewfinderView.
-         */
-        public static final int[] ViewfinderView = {
-            
-        };
-    };
 }
--- LemurCollections/src/com/crossjoinconsulting/lemur/UserMainActivity.java
+++ LemurCollections/src/com/crossjoinconsulting/lemur/UserMainActivity.java
@@ -10,6 +10,22 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonError.ErrorInfo;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.Json;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.books.v1.Books;
+import com.google.api.services.books.v1.Books.Volumes.List;
+import com.google.api.services.books.v1.model.Volume;
+import com.google.api.services.books.v1.model.VolumeSaleInfo;
+import com.google.api.services.books.v1.model.VolumeVolumeInfo;
+import com.google.api.services.books.v1.model.Volumes;
+
+import java.net.URLEncoder;
+import java.text.NumberFormat;
 
 public class UserMainActivity extends Activity implements OnClickListener
 {
@@ -18,6 +34,11 @@ public class UserMainActivity extends Activity implements OnClickListener
 	private Button btnScan;
 	private TextView tvContents;
 	private TextView tvFormat;
+	
+	private static final String API_KEY = "AIzaSyD6B91qHu4WtB54v2FtM1DRnCeqysaupSM";
+	
+	  private static final NumberFormat CURRENCY_FORMATTER = NumberFormat.getCurrencyInstance();
+	  private static final NumberFormat PERCENT_FORMATTER = NumberFormat.getPercentInstance();
 	
 	/** Called when the activity is first created. */
     @Override
@@ -60,16 +81,142 @@ public class UserMainActivity extends Activity implements OnClickListener
      */
     public void onActivityResult(int requestCode, int resultCode, Intent intent) 
     {
+    	String Contents = "";
+    	
 	    IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-	    if (scanResult != null) {
-	    	this.tvContents = (TextView)this.findViewById(R.id.tvContents);
-            this.tvFormat = (TextView)this.findViewById(R.id.tvFormat);
-            
-            tvContents.setText(scanResult.getContents());
-            tvFormat.setText(scanResult.getFormatName());
-            
+	    if (scanResult != null) 
+	    {
+	    	try
+	    	{
+		    	this.tvContents = (TextView)this.findViewById(R.id.tvContents);
+	            this.tvFormat = (TextView)this.findViewById(R.id.tvFormat);
+	            
+	            tvContents.setText(scanResult.getContents());
+	            tvFormat.setText(scanResult.getFormatName());
+	            Contents = scanResult.getContents();
+	    	}
+	    	catch(Throwable t)
+	    	{
+	    		
+	    	}
 	    }
 	    // else continue with any other code you need in the method
-	 
+	    {
+	    	
+	    }
+        try {
+        	JsonFactory jsonFactory = new JacksonFactory();
+        	
+          // Parse command line parameters into a query.
+          // Query format: "[<author|isbn|intitle>:]<query>"
+          String prefix = null;
+          String query = "";
+          prefix = "isbn:";
+          query = Contents;
+          
+          if (prefix != null) {
+            query = prefix + query;
+          }
+          try {
+            queryGoogleBooks(jsonFactory, query);
+            // Success!
+            return;
+          } catch (HttpResponseException e) {
+            if (!Json.CONTENT_TYPE.equals(e.getResponse().getContentType())) {
+              System.err.println(e.getResponse().parseAsString());
+            } else {
+              GoogleJsonError errorResponse = GoogleJsonError.parse(jsonFactory, e.getResponse());
+              System.err.println(errorResponse.code + " Error: " + errorResponse.message);
+              for (ErrorInfo error : errorResponse.errors) {
+                System.err.println(jsonFactory.toString(error));
+              }
+            }
+          }
+        } catch (Throwable t) {
+          t.printStackTrace();
+        }	 
 	 }
+    
+    private void queryGoogleBooks(JsonFactory jsonFactory, String query) throws Exception {
+        // Set up Books client.
+        final Books books = new Books(new NetHttpTransport(), jsonFactory);
+        books.setApplicationName("Google-BooksSample/1.0");
+        books.accessKey = API_KEY;
+
+        // Set query string and filter only Google eBooks.
+        System.out.println("Query: [" + query + "]");
+        List volumesList = books.volumes.list(query);
+        volumesList.filter = "ebooks";
+
+        // Execute the query.
+        Volumes volumes = volumesList.execute();
+        if (volumes.totalItems == 0 || volumes.items == null) {
+          System.out.println("No matches found.");
+          return;
+        }
+
+        // Output results.
+        for (Volume volume : volumes.items) {
+          VolumeVolumeInfo volumeInfo = volume.volumeInfo;
+          VolumeSaleInfo saleInfo = volume.saleInfo;
+          System.out.println("==========");
+          // Title.
+          System.out.println("Title: " + volumeInfo.title);
+          // Author(s).
+          java.util.List<String> authors = volumeInfo.authors;
+          if (authors != null && !authors.isEmpty()) {
+            System.out.print("Author(s): ");
+            for (int i = 0; i < authors.size(); ++i) {
+              System.out.print(authors.get(i));
+              if (i < authors.size() - 1) {
+                System.out.print(", ");
+              }
+            }
+            System.out.println();
+          }
+          // Description (if any).
+          if (volumeInfo.description != null && volumeInfo.description.length() > 0) {
+            System.out.println("Description: " + volumeInfo.description);
+          }
+          // Ratings (if any).
+          if (volumeInfo.ratingsCount != null && volumeInfo.ratingsCount > 0) {
+            int fullRating = (int) Math.round(volumeInfo.averageRating.doubleValue());
+            System.out.print("User Rating: ");
+            for (int i = 0; i < fullRating; ++i) {
+              System.out.print("*");
+            }
+            System.out.println(" (" + volumeInfo.ratingsCount + " rating(s))");
+          }
+          // Price (if any).
+          if ("FOR_SALE".equals(saleInfo.saleability)) {
+            double save = saleInfo.listPrice.amount - saleInfo.retailPrice.amount;
+            if (save > 0.0) {
+              System.out.print("List: " + CURRENCY_FORMATTER.format(saleInfo.listPrice.amount)
+                  + "  ");
+            }
+            System.out.print("Google eBooks Price: "
+                + CURRENCY_FORMATTER.format(saleInfo.retailPrice.amount));
+            if (save > 0.0) {
+              System.out.print("  You Save: " + CURRENCY_FORMATTER.format(save) + " ("
+                  + PERCENT_FORMATTER.format(save / saleInfo.listPrice.amount) + ")");
+            }
+            System.out.println();
+          }
+          // Access status.
+          String accessViewStatus = volume.accessInfo.accessViewStatus;
+          String message = "Additional information about this book is available from Google eBooks at:";
+          if ("FULL_PUBLIC_DOMAIN".equals(accessViewStatus)) {
+            message = "This public domain book is available for free from Google eBooks at:";
+          } else if ("SAMPLE".equals(accessViewStatus)) {
+            message = "A preview of this book is available from Google eBooks at:";
+          }
+          System.out.println(message);
+          // Link to Google eBooks.
+          System.out.println(volumeInfo.infoLink);
+        }
+        System.out.println("==========");
+        System.out.println(
+            volumes.totalItems + " total results at http://books.google.com/ebooks?q="
+            + URLEncoder.encode(query, "UTF-8"));
+      }
 }
